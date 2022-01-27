58eb15fd8fce2bf57e6894cabfa913ba79b9b0c8
Enda Phramoedya <45927738+EndaPhramoedya@users.noreply.github.com>
2022-01-27 13:31:25 +0700

    first init

---
+++ android/app/src/main/kotlin/com/toras/eight_barrels/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.toras.eight_barrels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:eight_barrels/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
